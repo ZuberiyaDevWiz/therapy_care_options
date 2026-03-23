af4f93651974a65d3d820d0776e71b839d37e639
Zuberiya <zuberiya139@gmail.com>
2026-03-23 21:10:14 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ import ContactPage from "./contact/page";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar />
+    <main className="flex justify-center">
+      {/* <Navbar />
       <Hero />
       <About />
       <Services />
       <Testimonials />
       <ContactPage/>
-      <Footer />
-      {/* <h1 className="">APPLICATION ERROR..</h1> */}
+      <Footer /> */}
+      <h1 className="">APPLICATION ERROR..</h1>
     </main>
   );
 }
